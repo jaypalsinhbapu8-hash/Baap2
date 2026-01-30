6486ffa1c55999d5f05f79dffe486cf13c653d21
jaypalsinhbapu8-hash <jaypalsinhbapu8@gmail.com>
2026-01-30 13:28:53 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,4 @@
-api_id = "27679306"
-api_hash = "d0f390f798eec4ddb9246a00a44b7a3f"
-bot_token = "8060931221:AAHlfa7nVaABOCNhVA3z8KC10blEB3yNBFM"
+api_id = "21201224"
+api_hash = "6279f81c0cc0ce6a42e155f3e6cfb7a6"
+bot_token = "8527452107:AAFzRx-GnUWv5qdFRbdVREmchn2oIYuK5TM"
+
